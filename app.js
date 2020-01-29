c0d40708da31e817d15c4e2d2f0e3c65df325083
Andrew Kristanto <akristanto6@gatech.edu>
2020-01-28 19:54:45 -0500

    added node.js for the backend

---
+++ app.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path')
+// var mysql = require('./js/mysql.js');
+const bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const validator  = require('express-validator')
+
+var app = express();
+
+app.use('/assets/css', express.static('css'));
+app.use(express.static('html'));
+app.use('/assets/js', express.static('js'));
+// app.use(validator());
+
+// var api = express.Router();
+
+// while (con == null){
+//   console.log('attempting sql connection')
+//   con = mysql.connect();
+// }
+
+//starts app
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+
+//loads home page on start up
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'./html/login.html'));
+});
+
+/* Page navigations */
+app.get("/logout", urlencodedParser,  function(req, res) {
+    res.sendFile(path.join(__dirname,'./html/login.html'));
+});
+
+//authenticates login
+app.post('/',urlencodedParser,  function(req, res) {
+  var username = req.body.email;
+  var password = req.body.password;
+  console.log("post received: Username: %s Password: %s", username, password);
+  res.sendFile(path.join(__dirname,'./html/home.html'));
+});

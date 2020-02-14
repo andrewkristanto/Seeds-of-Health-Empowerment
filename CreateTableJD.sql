6e37ad3aed65ef203d76e5d8d941868fbe1b9e58
Choe, Preston Keunha <prestonchoe@gatech.edu>
2020-02-14 15:45:58 -0500

    Add files via upload

--- CreateTableJD.sql
+++ CreateTableJD.sql
@@ -1,10 +1,10 @@
-drop database HWSG;
-CREATE database HWSG;
-use HWSG;
+drop database histor72_sapp;
+CREATE database histor72_sapp;
+use histor72_sapp;
 
 
 CREATE TABLE User (
-    userStatus VARCHAR(50) NOT NULL DEFAULT "Pending",
+	email VARCHAR(50) NOT NULL,
     password VARCHAR(200) NOT NULL,
 	firstName VARCHAR(50) NOT NULL,
 	lastName VARCHAR(50) NOT NULL,
@@ -12,24 +12,16 @@ CREATE TABLE User (
 	city VARCHAR(50) NOT NULL,
 	state VARCHAR(50) NOT NULL DEFAULT "GA",
 	zipcode VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL,
     phoneNumber VARCHAR(50) NOT NULL,
+    userStatus VARCHAR(50) NOT NULL DEFAULT "Pending",
     lastLogin Date NOT NULL,
 	PRIMARY KEY (email)
-);
-CREATE TABLE Employee (
-	email VARCHAR(50) NOT NULL,
-	PRIMARY KEY (email),
-	FOREIGN KEY (email)
-		REFERENCES User (email)
-			ON DELETE CASCADE
-			ON UPDATE CASCADE
 );
 CREATE TABLE Angel (
 	email VARCHAR(50) NOT NULL,
 	PRIMARY KEY (email),
 	FOREIGN KEY (email)
-		REFERENCES Employee (email)
+		REFERENCES User (email)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
@@ -51,19 +43,38 @@ CREATE TABLE Admin (
 	email VARCHAR(50) NOT NULL,
 	PRIMARY KEY (email),
 	FOREIGN KEY (email)
-		REFERENCES Employee (email)
+		REFERENCES User (email)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-CREATE TABLE Survey (
-	question VARCHAR(50) NOT NULL,
-	response VARCHAR(500) NOT NULL,
+CREATE TABLE SurveyQuestions (
+	question VARCHAR(100) NOT NULL,
+	releaseDate Date NOT NULL,
 	questionType VARCHAR(50) NOT NULL,
+	PRIMARY KEY (question, releaseDate)
+);
+CREATE TABLE SurveyResponses (
+	question VARCHAR(100) NOT NULL,
 	releaseDate Date NOT NULL,
     email VARCHAR(50) NOT NULL,
-	PRIMARY KEY (email),
+	response VARCHAR(200) NOT NULL,
+	PRIMARY KEY (question, response, releaseDate, email),
+	FOREIGN KEY (question, releaseDate)
+		REFERENCES SurveyQuestions (question, releaseDate)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE,
 	FOREIGN KEY (email)
-		REFERENCES userww2 (email)
+		REFERENCES User (email)
+			ON DELETE CASCADE
+			ON UPDATE CASCADE
+);
+CREATE TABLE SurveyAnswers (
+	question VARCHAR(50) NOT NULL,
+	releaseDate Date NOT NULL,
+	answerChoice VARCHAR(50) NOT NULL,
+	PRIMARY KEY (question, releaseDate, answerChoice),
+	FOREIGN KEY (question, releaseDate)
+		REFERENCES SurveyQuestions (question, releaseDate)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
@@ -113,19 +124,19 @@ CREATE TABLE Comments (
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-CREATE TABLE notifications (
+CREATE TABLE Notifications (
 	email VARCHAR(50) NOT NULL,
-	role ENUM('Angel', 'Gardener', 'All') NOT NULL,
+	role ENUM('Angel', 'Gardener', 'All', 'Private') NOT NULL,
 	notifDate Date NOT NULL,
     notifTime Time NOT NULL,
-	content VARCHAR(300) NOT NULL,
+	content VARCHAR(250) NOT NULL,
 	PRIMARY KEY (email, notifDate, notifTime, content),
     FOREIGN KEY (email)
 		REFERENCES User (email)
 			ON DELETE CASCADE
 			ON UPDATE CASCADE
 );
-CREATE TABLE notificationSettings (
+CREATE TABLE NotificationSettings (
 	email VARCHAR(50) NOT NULL,
 	frequency ENUM('Immediate', 'Every 8 hours', 'Once a day') NOT NULL,
 	method ENUM('Phone', 'Email', 'Email and Phone') NOT NULL,

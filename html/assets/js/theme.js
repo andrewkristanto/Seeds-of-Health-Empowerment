49a81c012359ca2f6ee7ed3e16dacaf0ce001f66
andrewkristanto <andrewkristanto555@gmail.com>
2020-04-20 15:32:23 -0400

    added date to survey data dropdown

--- html/assets/js/theme.js
+++ html/assets/js/theme.js
@@ -46,3 +46,41 @@ function formatDate(date) {
 
     return date.join(" ");
 }
+
+function formatShortDate(date){
+    date = date.split("T");
+    date[0] = date[0].slice(5, 10) + "-" + date[0].slice(0, 4);
+    date[1] = date[1].slice(0, 5);
+
+    var hour = date[1].slice(0, 2) - 3;
+
+    if (hour < 0) {
+        hour = hour + 24;
+        month = date[0].slice(0, 2);
+        day = date[0].slice(3, 5) - 1;
+        year = date[0].slice(6, 10);
+        
+        if (day <= 0) {
+            month = (month - 1) < 10 ? "0" + (month - 1) : (month - 1);
+            if (month <= 0) {
+                month = 12;
+                year = year - 1;
+            }
+            day = monthDays[month];
+        }
+
+        date[0] = month + "-" + day + "-" + year;
+    }
+    if (hour > 12) {
+        hour = (hour - 12) < 10 ? "0" + (hour - 12) : (hour - 12);
+        date[1] = hour + date[1].slice(2, 5) + " PM";
+    } else {
+        if (hour == 0) {
+            hour = 12
+        }
+        hour = hour < 10 ? "0" + hour : hour;
+        date[1] = hour + date[1].slice(2, 5) +  " AM";
+    }
+
+    return date[0]
+}
